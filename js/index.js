2c705d4133a297ac5d90a026a0180a949cdb9527
suhaibileeye <suhaibileeye@gmail.com>
2021-02-15 09:11:04 -0800

    push main

--- js/index.js
+++ js/index.js
@@ -5,9 +5,22 @@
 /* 
 
 1. Start creating three prompts, the first two for the two numbers and the second one for the operation
+*/
+alert('welcome to iqra islamic school');
+alert(2021)
+/*
 2. Also declare another variable for the result.
+*/
+let myInt = alert("welcome to iqra islamic school");
+let myFloat = (alert("2021"));
+myFloat;
+
+/*
 3. Using conditional statement, check if the operation is addition, subtraction, division or multipilcation
 4. Add the output to the result variable you created.
-5. Console.log the result.
+5. Console.log the result. */
 
-*/
+console.log(alert)
+
+
+
